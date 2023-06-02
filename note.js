343cffb2e1494a21babb0c1714c2a885d8e67bb8
Mr.Choi <teemskorea@naver.com>
2023-06-03 04:20:50 +0900

    단어 리스트 추가, 로컬 스토리지에 저장기능 추가

--- note.js
+++ note.js
@@ -7,3 +7,16 @@ var newWords = {
 
   document.querySelector("#add-button").addEventListener("click", addWord);
   document.querySelector("#search-input").addEventListener("input", searchWord);
+
+  function addWord(event) {
+    if (validate.value === "") {
+      alert("Please insert your word of the day");
+    } else {
+      event.preventDefault();
+      var d1 = document.getElementById('card');
+      d1.insertAdjacentHTML('beforeend', '<li><div><input class="card-title" value="' + newWords.wordH2Input.value + '" disabled><input class="card-text" value="' + newWords.wordMeaningInput.value + '" disabled></div><div><button class="edit">edit</button><button class="update">update</button><button class="delete">delete</button></div></li>');
+      localStorage.setItem("word", newWords.wordH2Input.value);
+      document.getElementById("english").value = "";
+      document.getElementById("korean").value = "";
+    }
+  }
